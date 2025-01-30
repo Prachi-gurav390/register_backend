cd3df553ee5a7b46db7deb3eaae2eaa4322c993e
Prachi3045 <pikugurav007@gmail.com>
2025-01-30 13:24:11 +0530

    Updated server to handle cors for PATCH requests

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,PATCH");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
